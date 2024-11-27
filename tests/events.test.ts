0e826cd8e17d45de2877a7e6ad7f80ca98b204d2
Ayrton Tales Silva do Nascimento <ayrtontsn@hotmail.com>
2024-11-27 11:19:58 -0300

    Test: criate tests get/events/id, put/events/id and delete/events/id

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -9,18 +9,18 @@ const api = supertest(app)
 
 beforeEach(async () => {
     await prisma.event.deleteMany();
-  });
+});
 
-  describe("post /events",()=>{
-    it("Criar eventos",async ()=>{
+describe("post /events", () => {
+    it("Criar eventos", async () => {
         //criar cenário
-        const {status, body} = await api.post("/events").send({
+        const { status, body } = await api.post("/events").send({
             name: faker.company.name(),
             date: faker.date.future()
         })
 
-        expect (status).toBe(201)
-        expect (body).toEqual(
+        expect(status).toBe(201)
+        expect(body).toEqual(
             expect.objectContaining({
                 id: expect.any(Number),
                 name: expect.any(String),
@@ -30,13 +30,68 @@ beforeEach(async () => {
     })
 })
 
-describe("get /events",()=>{
-    it("Retornar todos os eventos",async ()=>{
+describe("get /events", () => {
+    it("Retornar todos os eventos", async () => {
         //criar cenário
         await create_event(faker.company.name(), faker.date.future())
         await create_event(faker.company.name(), faker.date.future())
-        
-        const {status, body} = await api.get("/events");
-        expect (status).toBe(200)
+
+        const { status, body } = await api.get("/events");
+        expect(status).toBe(200)
+        expect(body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    name: expect.any(String),
+                    date: expect.any(String)
+                })
+            ])
+        )
     })
+
+    it("Retornar todos eventos por id", async () => {
+        //criar cenário
+        const { id } = await create_event(faker.company.name(), faker.date.future())
+
+        const { status, body } = await api.get(`/events/${id}`);
+        expect(status).toBe(200)
+        expect(body).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                name: expect.any(String),
+                date: expect.any(String)
+            })
+        )
+    })
+
+})
+
+describe("put /events", () => {
+    it("editar eventos por id", async () => {
+        const { id } = await create_event(faker.company.name(), faker.date.future())
+
+        const { status, body } = await api.put(`/events/${id}`).send({
+            name: faker.company.name(),
+            date: faker.date.future()
+        })
+        expect(status).toBe(200)
+        expect(body).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                name: expect.any(String),
+                date: expect.any(String)
+            })
+        )
+    })
+
+})
+
+describe("delete /events", () => {
+    it("deletar eventos por id", async () => {
+        const { id } = await create_event(faker.company.name(), faker.date.future())
+
+        const { status } = await api.delete(`/events/${id}`)
+        expect(status).toBe(204)
+    })
+
 })
